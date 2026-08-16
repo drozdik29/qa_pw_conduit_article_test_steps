b0707668237af353babeba023f34d2c2329d7117
Max Drozdovsky <drozdovskiimax@gmail.com>
2026-08-16 21:23:45 +0300

    Update createArticleWithoutRequiredFields.spec.js

--- tests/createArticle/createArticleWithoutRequiredFields.spec.js
+++ tests/createArticle/createArticleWithoutRequiredFields.spec.js
@@ -4,8 +4,8 @@ import { HomePage } from '../../src/pages/HomePage';
 import { CreateArticlePage } from '../../src/pages/CreateArticlePage';
 import { faker } from '@faker-js/faker';
 
-let homePage;
-let createArticlePage;
+let homePage: HomePage;
+let createArticlePage: CreateArticlePage;
 
 test.beforeEach(async ({ page }) => {
   const signUpPage = new SignUpPage(page);
@@ -18,19 +18,31 @@ test.beforeEach(async ({ page }) => {
     password: faker.internet.password(),
   };
 
-  await signUpPage.open();
-  await signUpPage.fillUsernameField(user.username);
-  await signUpPage.fillEmailField(user.email);
-  await signUpPage.fillPasswordField(user.password);
-  await signUpPage.clickSignUpButton();
-  await homePage.assertYourFeedTabIsVisible();
+  await test.step('Register a new user', async () => {
+    await signUpPage.open();
+    await signUpPage.fillUsernameField(user.username);
+    await signUpPage.fillEmailField(user.email);
+    await signUpPage.fillPasswordField(user.password);
+    await signUpPage.clickSignUpButton();
+  });
+
+  await test.step('Verify user is redirected to the home page', async () => {
+    await homePage.assertYourFeedTabIsVisible();
+  });
 });
 
-test('Creat an article without required fields', async () => {
-  await homePage.clickNewArticleLink();
+test('Create an article without required fields', async () => {
+  await test.step('Navigate to the create article page', async () => {
+    await homePage.clickNewArticleLink();
+  });
 
-  await createArticlePage.clickPublishArticleButton();
-  await createArticlePage.assertErrorMessageContainsText(
-    'Article title cannot be empty',
-  );
+  await test.step('Click publish button with empty fields', async () => {
+    await createArticlePage.clickPublishArticleButton();
+  });
+
+  await test.step('Assert error message for empty title', async () => {
+    await createArticlePage.assertErrorMessageContainsText(
+      'Article title cannot be empty',
+    );
+  });
 });
